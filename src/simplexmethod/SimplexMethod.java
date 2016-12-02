21044a406467e0b0127a121b70bbd00ee0775b6f
Andrey Chegrinets <chegrinets.andrey@gmail.com>
2016-12-02 04:09:44 +0000

    Add implementation of simplex method

--- src/simplexmethod/SimplexMethod.java
+++ src/simplexmethod/SimplexMethod.java
@@ -5,10 +5,167 @@
  */
 package simplexmethod;
 
+import java.util.*;
+
 /**
  *
  * @author jo1nsaint
  */
 public class SimplexMethod {
+    
+
+    private double matr[][];
+    private double function[];
+    private boolean flag = false;
+    
+    public SimplexMethod() {
+    }
+
+    public SimplexMethod(double[][] matr, double[] function) {
+
+        this.matr = matr;
+        this.function = function;
+        
+    }
+
+    public double[] findPlan() {
+        int count = 0;
+        double[] arrayIndexPlan;
+        double sumt;
+        double[] sum = new double[8];
+        for (int z = 0; z < 4; z++) {
+            for (int j = 0; j < 7; j++) {
+                if (matr[z][j] == 1) {
+                    sumt = 0.0;
+                    for (int k = 0; k < 4; k++) {
+                        if (k != z) {
+                            sumt += Math.abs(matr[k][j]);
+                        }
+                    }
+                    if (sumt == 0) {
+                        count++;
+                    }
+                }
+            }
+        }
+        arrayIndexPlan = new double[count];
+        int i = 0;
+        for (int z = 0; z < 4; z++) {
+            for (int j = 0; j < 7; j++) {
+                if (matr[z][j] == 1) {
+                    sumt = 0.0;
+                    for (int k = 0; k < 4; k++) {
+                        if (k != z) {
+                            sumt += Math.abs(matr[k][j]);
+                        }
+                    }
+                    if (sumt == 0) {
+                        arrayIndexPlan[i] = j;
+                        i++;
+                    }
+                }
+            }
+        }
+        return arrayIndexPlan;
+    }
+
+    public double[] findDelta(double[] currentPlan) {
+        int length = currentPlan.length;
+        double[] C = new double[length];
+        double[] delta = new double[8];
+        for (int z = 0; z < length; z++) {
+            C[z] = function[(int) currentPlan[z]];
+        }
+        for (int k = 0; k < 8; k++) {
+            double temp = 0.0;
+            for (int j = 0; j < length; j++) {
+                temp += C[j] * matr[j][k];
+            }
+            delta[k] = temp - function[k];
+        }
+        return delta;
+    }
+
+    public void checkPlan(double[] currentDelta) {
+        double minElement = currentDelta[0];
+        double[] nextDelta = new double[8];
+        int indexElem = 0;
+        
+        for (int k = 1; k < 8; k++) {
+            if (minElement > currentDelta[k]) {
+                minElement = currentDelta[k];
+                indexElem = k;
+            }
+        }
+        double[] minOper = new double[4];
+        int indexNewPlan = 0;
+        if (minElement < 0) {
+            for (int k = 0; k < 4; k++) {
+                if (matr[k][indexElem] > 0) {
+                    minOper[k] = matr[k][7] / matr[k][indexElem];
+                } else {
+                    minOper[k] = 1000;
+                }
+            }
+            double minOperation = minOper[0];
+            for (int k = 1; k < 4; k++) {
+                if (minOper[k] < minOperation) {
+                    minOperation = minOper[k];
+                    indexNewPlan = k;
+                }
+            }
+            double[][] newMatr = new double[4][8];
+            for (int k = 0; k < 4; k++) {
+                for (int j = 0; j < 8; j++) {
+                    if (k == indexNewPlan) {
+                        newMatr[indexNewPlan][j] = matr[indexNewPlan][j] / matr[indexNewPlan][indexElem];
+                    } else {
+                        newMatr[k][j] = matr[k][j] - (matr[indexNewPlan][j] / matr[indexNewPlan][indexElem]) * matr[k][indexElem];
+                    }
+                }
+            }
+            matr = newMatr;
+            
+        } else {
+            flag = true;
+        }
+    }
+
+    public boolean getFlag() {
+        return flag;
+    }
+
+    public double mainResult(double[] result) {
+        double mainFunct = 0.0;
+        for (int k = 0; k < 7; k++) {
+            mainFunct += function[k] * result[k];
+        }
+        return mainFunct;
+    }
+
+    public double[] justDoIt() {
+        double[] currentDelta;
+        double[] currentPlan = null;
+        int counter = 0;
+        while (!getFlag()) {
+            currentPlan = findPlan();
+            currentDelta = findDelta(currentPlan);
+            checkPlan(currentDelta);
+            counter++;
+            if(counter > 1000) {
+                throw new NullPointerException();
+            }
+        }
+        double[] result = new double[7];
+        for (int j = 0; j < 4; j++) {
+            for (int k = 0; k < 7; k++) {
+                result[k] = 0;
+            }
+        }
+        for (int k = 0; k < currentPlan.length; k++) {
+            result[(int) currentPlan[k]] = matr[k][7];
+        }
+        return result;
+    }
     
 }
--- src/simplexmethod/Window.java
+++ src/simplexmethod/Window.java
@@ -6,8 +6,11 @@
 package simplexmethod;
 
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Arrays;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -27,16 +30,16 @@ public class Window {
     
     private JFrame mainWindow;
     
-    private JPanel entryFunctions;
+    //private JPanel entryFunctions;
     private JPanel logPanel;
+    private JPanel panel; 
     
-    
-    private Integer m, n; // m - columns, n - rows (entryValues)
+    private Integer m, n; // n - количество переменных, m - количество ограничений
     
     private JTextField entryValue; // Количество переменных
     private JTextField entryRestrictions;// количество ограничений
-    private JTextField x;
-    private JTextField xIJ; // значения для матрицы свободных членов и переменных
+    
+    private JTextField xIJ = null; // значения для матрицы свободных членов и переменных
        
     private JTextArea log;
     private JScrollPane scroll; // scroll for log
@@ -49,12 +52,19 @@ public class Window {
     private JButton btnOk1;
     
     private JComboBox minOrMax;
-    private final String[] minOrMaxValue = {"min", "max"};
+    private final String[] minOrMaxValue = {"choise", "min", "max"};
     
     private int number;
-    private Integer minOrMaxChoise;// save minOrMax value
+    private Integer minOrMaxChoise = 2;// save minOrMax value
+    
+    static JTextField[] linksFunctionValue = new JTextField[8];
+    static JTextField[][] linksMatrValue = new JTextField[4][8];
+    
+    private double matr[][];
+    private double functionVector[];
     
     public Window(){
+        
         mainWindow = new JFrame();
         mainWindow.setTitle("Simplex Method");
         mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -96,7 +106,7 @@ public class Window {
                             try {
                                 //TODO: Доделать проверку
                                 if((entryValue.getText().trim().length() <= 0)
-                                        && (Integer.valueOf(entryValue.getText().toString()) > 4)){
+                                        && (Integer.valueOf(entryValue.getText()) > 4)){
                                             //&& (Integer.valueOf(entryRestrictions.getText().toString()) > 4)){
                                 
                                     throw new IllegalArgumentException("Выполз за пределы");
@@ -104,8 +114,8 @@ public class Window {
                                 }    
                                 else{
                                     
-                                    n = Integer.valueOf(entryValue.getText().toString());
-                                    m = Integer.valueOf(entryRestrictions.getText().toString());
+                                    n = Integer.valueOf(entryValue.getText());
+                                    m = Integer.valueOf(entryRestrictions.getText());
                                 
                                     System.out.println("Check values : " + m + " " + n);
                                 
@@ -113,7 +123,7 @@ public class Window {
                                     mainWindow.repaint();
                                     
                                 }
-                            } catch (Exception ex) {
+                            } catch (IllegalArgumentException ex) {
                                 System.out.println("Err-1 in btn1 :" + ex);
                             }
                         } 
@@ -154,9 +164,9 @@ public class Window {
      
     private JPanel addFunctionsField(){
         
-       JPanel panel = new JPanel(null);
+       panel = new JPanel(null);
        panel.setBorder(new TitledBorder("Ввод значений"));
-       panel.setBounds(20, 200, 400, 270);
+       panel.setBounds(20, 170, 400, 300);
        
        textFunction = new  JLabel("F(x) = ");
        textFunction.setBounds(30, 15, 150, 30);
@@ -165,7 +175,9 @@ public class Window {
        
        number = 1;
        
-        for (int i = 0; i < n; i++) {
+       JTextField x;
+       
+       for (int i = 0; i < n; i++) {
             
             x = new JTextField();
             x.setBounds(75 + i * 60, 20, 40, 20);
@@ -174,19 +186,22 @@ public class Window {
             printX.setBounds(118 + i * 60, 20, 20, 20);
             printX.setText("x" + number);
             
+            linksFunctionValue[i] = x;
+            
             panel.add(x);
             panel.add(printX);
+            
             number++;
-        }
+       }
         
 
         minOrMax = new JComboBox(minOrMaxValue);
-        minOrMax.setBounds(330, 20, 35, 20);
+        minOrMax.setBounds(330, 20, 55, 20);
         minOrMax.addActionListener((e) -> {
             
             int choise = minOrMax.getSelectedIndex();
             
-            System.out.println("Selected choise :  " + choise + " //if 0 - min, 1 - max");
+            System.out.println("Selected choise :  " + choise + " //if 1 - min, 2 - max");
             
             minOrMaxChoise = choise;
         });
@@ -198,7 +213,7 @@ public class Window {
         example1.setBounds(20, 50, 300, 20);
         
         example2 = new JLabel();
-        example2.setText("b = x1 + x2 + ... + xn");
+        example2.setText("x1 + x2 + ... + xn = b");
         example2.setBounds(60, 70, 200, 20);
         
         panel.add(example1);
@@ -211,8 +226,9 @@ public class Window {
             for(int j = 0; j < n + 1; j++){
                 
                 xIJ = new JTextField();
-                //xIJ.setText("x"+i+j);
                 xIJ.setBounds(60 + j * 60, 100 + i * 40, 40, 20);
+                
+                linksMatrValue[i][j] = xIJ;
                 
                 panel.add(xIJ);
             }
@@ -220,12 +236,70 @@ public class Window {
        
         JButton btn2 = new JButton();
         btn2.setText("Решить");
-        btn2.setBounds(60, 220, 140, 30);
+        btn2.setBounds(200, 260, 140, 30);
+        
+        btn2.addActionListener((ActionEvent e) -> {
+            matr = new double [4][8];
+            functionVector = new double[8];
+            initArr(matr, functionVector);
+            getValue(matr, functionVector);
+            printArr(matr);
+            System.out.println(Arrays.toString(functionVector));
+            SimplexMethod simplex = new SimplexMethod(matr, functionVector);
+            
+            double[] functionResult = simplex.justDoIt();
+            double result = simplex.mainResult(functionResult);
+            System.out.println(Arrays.toString(functionResult));
+            System.out.println(result);
+       });
         
         panel.add(btn2);
         
         return panel;
     }
     
+    public void printArr(double[][] arr) {
+        for(int i = 0; i < 4; i++) {
+            for(int j = 0; j < 8; j++) {
+                System.out.print("[ " + arr[i][j] + " ]");
+            }
+            System.out.println();
+        }
+    }
+    
+    public void getValue(double [][] arr, double [] function) {
+        
+        int temp = 0;
+        
+        for(int i = 0; i < m; i++) {
+            arr[i][7] = Double.valueOf(linksMatrValue[i][n].getText());
+            for(int j = 0; j < n; j++) {
+                arr[i][j] = Double.valueOf(linksMatrValue[i][j].getText());
+                function[j] = Double.valueOf(linksFunctionValue[j].getText());
+                
+            }
+            
+        }
+        if(minOrMaxChoise == 1) {
+            for(int i = 0; i < n; i++){
+                
+                function[i] = function[i] * (-1);
+            }
+        }
+        
+            for(int i = 0; i < m; i++){
+                    matr[i][n] = 1;
+                    n++;
+                }
+            }
+    
+    public void initArr(double [][] matr, double[] function) {
+        for (int k = 0; k < m; k++) {
+            function[k] = 0;
+            for (int j = 0; j < 8; j++) {
+                matr[k][j] = 0;
+            }
+        }
+    }
     
 }
